bfc0016e78db96d6358b09f3060fa9db16aa3c96
dibh7710 <dibh7710@colorado.edu>
2021-02-03 19:35:34 -0700

    Pair 1 commit

--- src/edu/colorado/binarybuffs/Ship.java
+++ src/edu/colorado/binarybuffs/Ship.java
@@ -3,6 +3,9 @@ package edu.colorado.binarybuffs;
 // TODO: practice good OO design
 public class Ship {
     private String name;
+
+    //Team BinaryBuffs, pair 1 was here
+
     // TODO: create appropriate getter and setter methods
     // TODO: Understand encapsulation
     // TODO: Understand what these todo comments mean
@@ -10,4 +13,7 @@ public class Ship {
     public  void show()     {     // dunno why this is here maybe it is just an example method
         System.out.println("IF you can't see this then something is severely wrong!!");
     }
+
+    //
+
 }
