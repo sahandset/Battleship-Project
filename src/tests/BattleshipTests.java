354aae59edc3f85d5c3ca747f06c6628e3959c12
dibh7710 <dibh7710@colorado.edu>
2021-02-18 13:50:28 -0700

    Created test suite and made first test

---
+++ src/tests/BattleshipTests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class BattleshipTests {
+
+    @Test
+    public void checkGrid() {
+        Grid myGrid = new Grid(); //testing the creation of grid
+    }
+}
