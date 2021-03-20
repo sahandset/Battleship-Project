e7655700da1e5c0d2a7385ff242c891698a10072
tago3904 <tago3904@colorado.edu>
2021-03-19 22:29:51 -0600

    Re-implemented code for Map class

--- src/edu/colorado/binarybuffs/Map.java
+++ src/edu/colorado/binarybuffs/Map.java
@@ -1,5 +1,6 @@
 package edu.colorado.binarybuffs;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
@@ -8,11 +9,11 @@ public abstract class Map {
     newGrid offensiveGrid;
     newGrid defensiveGrid;
 
-    Hashtable<Ship, Coordinate> captainsQuarters = new Hashtable<>();
+    Hashtable<newShip, Coordinate> captainsQuarters = new Hashtable<>();
 
-    Hashtable<Ship, ArrayList<Coordinate>> shipCoordinates = new Hashtable<>();
+    Hashtable<newShip, ArrayList<Coordinate>> shipCoordinates = new Hashtable<>();
 
-    Hashtable<Ship, ArrayList<Coordinate>> shipDirections = new Hashtable<>();
+    Hashtable<newShip, String> shipDirections = new Hashtable<>();
     
     public Map(){
         offensiveGrid = new newGrid();
@@ -28,14 +29,38 @@ public abstract class Map {
         //validated it
         //boolean ship_is_legit ...
         //if(ship_is_legit){
-        //set sell status == 1 for each in coords
+        //set cell status == 1 for each in coords
         //add to hashtable of shipCoordinates
         //add capts quarts to captainsQuarters
+        boolean ship_is_legit = validateShip(coords);
+
+        if (ship_is_legit == true){
+            for (int i = 0; i < coords.size(); i++) {
+                defensiveGrid.setCellStatus(1, coords.get(i).x, coords.get(i).y);
+            }
+            shipCoordinates.put(ship, coords);
+            captainsQuarters.put(ship, captsQuart);
+            shipDirections.put(ship, direction);
+        }
+
     }
 
     //validateShip(coords)
-    // went through each cord and checked out of bounds
-    // checked cell status
-    // returned true or false
+        // went through each coord and checked out of bounds
+        // checked cell status
+        // returned true or false
+    public boolean validateShip(ArrayList<Coordinate> coords){
+
+        for (int i = 0; i < coords.size(); i++) {
+            if (coords.get(i).x < 0 || coords.get(i).y < 0 || coords.get(i).x > 10 || coords.get(i).y > 10)
+            {
+                return false;
+            }
+            if (defensiveGrid.checkCellStatus(coords.get(i).x, coords.get(i).y) == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
