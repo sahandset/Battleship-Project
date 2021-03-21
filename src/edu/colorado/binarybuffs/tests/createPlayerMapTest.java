5e550c35b04f6d1cfdff85773d30ea617e97b7f0
Sahand Setareh <sahand.set@gmail.com>
2021-03-21 11:07:52 -0600

    Testing refactored Maps created by player - tests passed

--- src/edu/colorado/binarybuffs/tests/createPlayerMapTest.java
+++ src/edu/colorado/binarybuffs/tests/createPlayerMapTest.java
@@ -12,7 +12,7 @@ public class createPlayerMapTest {
         OceanMap om1 = new OceanMap();
         UnderwaterMap uw1 = new UnderwaterMap();
 
-        assertEquals(uw1.getName(), player1.getPlayerMaps().get(0).getName());
-        assertEquals(om1.getName(), player1.getPlayerMaps().get(1).getName());
+        assertEquals(om1.getName(), player1.getPlayerMaps().get(0).getName());
+        assertEquals(uw1.getName(), player1.getPlayerMaps().get(1).getName());
     }
 }
