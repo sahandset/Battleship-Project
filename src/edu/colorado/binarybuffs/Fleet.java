1319b0bea45b9c5aa6bd594fd4946fc29c5beaa3
dibh7710 <dibh7710@colorado.edu>
2021-04-13 16:00:10 -0600

    Created test for Fleet class

---
+++ src/edu/colorado/binarybuffs/Fleet.java
@@ -0,0 +1,6 @@
+package edu.colorado.binarybuffs;
+
+import java.util.ArrayList;
+
+public class Fleet{
+}
---
+++ src/edu/colorado/binarybuffs/tests/FleetTest.java
@@ -0,0 +1,11 @@
+package edu.colorado.binarybuffs.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class FleetTest {
+    @Test
+    public void CreateRandomFleet() {
+        Fleet myFleet = new Fleet();
+        print(myFleet.getShips());
+    }
+}
