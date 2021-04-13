67c1e487d3f09351754c09eee1828b1b7f27343f
dibh7710 <dibh7710@colorado.edu>
2021-04-13 17:10:14 -0600

    Created constructor for Fleet class

--- src/edu/colorado/binarybuffs/Fleet.java
+++ src/edu/colorado/binarybuffs/Fleet.java
@@ -3,4 +3,17 @@ package edu.colorado.binarybuffs;
 import java.util.ArrayList;
 
 public class Fleet{
+    public Fleet(String mapType){
+        if (mapType == "UnderwaterMap"){
+            Submarine sub = new Submarine();
+        }
+        else if (mapType == "OceanMap"){
+            Minesweeper mine = new Minesweeper();
+            Battleship battle = new Battleship();
+            Destroyer dest = new Destroyer();
+        }
+        else if (mapType == "SpaceMap"){
+            Spaceshuttle shuttle = new Spaceshuttle();
+        }
+    }
 }
