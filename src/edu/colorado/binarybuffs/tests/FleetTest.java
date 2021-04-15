0faf1547b691001e91cb00e8067669ef0b84d754
dibh7710 <dibh7710@colorado.edu>
2021-04-14 19:11:17 -0600

    Updated fleet class, test not working as expected

--- src/edu/colorado/binarybuffs/tests/FleetTest.java
+++ src/edu/colorado/binarybuffs/tests/FleetTest.java
@@ -1,11 +1,12 @@
 package edu.colorado.binarybuffs.tests;
 
+import edu.colorado.binarybuffs.*;
 import org.junit.jupiter.api.Test;
 
 public class FleetTest {
     @Test
     public void CreateRandomFleet() {
-        Fleet myFleet = new Fleet();
-        print(myFleet.getShips());
+        Fleet myFleet = new Fleet("OceanMap");
+        System.out.println(myFleet.getShips());
     }
 }
