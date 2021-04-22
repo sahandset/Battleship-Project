8dfe99c72cee233cbc9443b3a141154812fa7250
dibh7710 <dibh7710@colorado.edu>
2021-04-21 19:00:01 -0600

    Created extra test for Fleet class to handle edge cases: All tests passed!

--- src/edu/colorado/binarybuffs/tests/FleetTest.java
+++ src/edu/colorado/binarybuffs/tests/FleetTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FleetTest {
     @Test
-    public void CreateRandOceanFleet() {
+    public void CreateRandFleet() {
         Minesweeper mine = new Minesweeper();
         Battleship battle = new Battleship();
         Destroyer dest = new Destroyer();
@@ -27,25 +27,8 @@ public class FleetTest {
         assertEquals("Minesweeper", ships.get(0).getName());
     }
 
-//    @Test
-//    public void CreateRandUnderwaterFleet() {
-//        Fleet myFleet = new Fleet("UnderwaterMap");
-//        ArrayList<newShip> ships = myFleet.getShips();
-//        assertEquals(1, ships.size());
-//        assertEquals("Submarine", ships.get(0).getName());
-//
-//    }
-//
-//    @Test
-//    public void CreateRandSpaceFleet() {
-//        Fleet myFleet = new Fleet("SpaceMap");
-//        ArrayList<newShip> ships = myFleet.getShips();
-//        assertEquals(1, ships.size());
-//        assertEquals("Spaceshuttle", ships.get(0).getName());
-//    }
-
     @Test
-    public void PlaceRandOceanFleet() {
+    public void PlaceRandFleet() {
         Minesweeper mine = new Minesweeper();
         Battleship battle = new Battleship();
         Destroyer dest = new Destroyer();
@@ -67,5 +50,36 @@ public class FleetTest {
         assertEquals(3, shipsOnOceanMap.size());
         assertEquals(1, shipsOnSpaceMap.size());
         assertEquals(1, shipsOnUnderwaterMap.size());
+    }
+
+    @Test
+    public void AftermathOfRandFleet() {
+        Minesweeper mine = new Minesweeper();
+        Battleship battle = new Battleship();
+        Destroyer dest = new Destroyer();
+        Submarine sub = new Submarine();
+        Spaceshuttle shuttle = new Spaceshuttle();
+        ArrayList<newShip> myShips = new ArrayList<>();
+        myShips.add(mine);
+        myShips.add(battle);
+        myShips.add(dest);
+        myShips.add(sub);
+        myShips.add(shuttle);
+        Fleet myFleet = new Fleet(myShips);
+
+        newPlayer player1 = new newPlayer("Tanvi");
+        newPlayer player2 = new newPlayer("Sahand");
+
+        myFleet.placeFleet(player1);
+        ArrayList<newShip> shipsOnOceanMap = player1.getPlayerMaps().get(0).getExistingShips();
+        ArrayList<newShip> shipsOnUnderwaterMap = player1.getPlayerMaps().get(1).getExistingShips();
+        ArrayList<newShip> shipsOnSpaceMap = player1.getPlayerMaps().get(2).getExistingShips();
+        assertEquals(3, shipsOnOceanMap.size());
+        assertEquals(1, shipsOnSpaceMap.size());
+        assertEquals(1, shipsOnUnderwaterMap.size());
+
+        newShip shipy = player1.getPlayerMaps().get(0).getExistingShips().get(0);
+        ArrayList<Coordinate> coordinatesOfShipy = player1.getPlayerMaps().get(0).getShipCoordinatesHash().get(shipy);
+        player2.useWeapon(0, coordinatesOfShipy.get(0).x, coordinatesOfShipy.get(0).y, player1, 0, 2);
     }
 }
