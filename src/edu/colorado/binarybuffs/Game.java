d501ed9155587def1f1bf39b7d7b02b76d5c3ca4
tago3904 <tago3904@colorado.edu>
2021-04-21 22:49:08 -0600

    Changes to game class implementation.

--- src/edu/colorado/binarybuffs/Game.java
+++ src/edu/colorado/binarybuffs/Game.java
@@ -1,4 +1,5 @@
 package edu.colorado.binarybuffs;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -23,18 +24,35 @@ public class Game {
 
     public void startGame() {
 
-        int turn_number = 1;
+        int turn_number = 0;
         boolean game_has_ended = false;
 
-
         while(!game_has_ended) {
-
+            if (turn_number % 2 == 0) {
+                game_has_ended = turn(player1, player2);
+            }
+            else {
+                game_has_ended = turn(player2, player1);
+            }
             turn_number++;
         }
     }
 
-    public void turn(newPlayer current_player, newPlayer opponent_player) { //return bool?
-        displayStartingMenu(current_player);
+    public boolean turn(newPlayer current_player, newPlayer opponent_player) {
+        boolean game_has_ended = false;
+        newShip sweeper = new Minesweeper();
+        newShip dest = new Destroyer();
+        newShip bat = new Minesweeper();
+        newShip sub = new Submarine();
+        newShip shut = new Spaceshuttle();
+        ArrayList<newShip> ship_objects = new ArrayList<>();
+        ship_objects.add(sweeper);
+        ship_objects.add(bat);
+        ship_objects.add(dest);
+        ship_objects.add(sub);
+        ship_objects.add(shut);
+
+        displayStartingMenu(current_player, ship_objects);
         System.out.println(current_player.getName() + "'s turn");
         createDisaster(current_player);
         showTurnMenu();
@@ -49,12 +67,9 @@ public class Game {
                 int map_choice = displayMapMenu(current_player);
                 current_player.player_maps.get(map_choice).printDefensiveGrid();
                 current_player.player_maps.get(map_choice).printOffensiveGrid();
-                turn(current_player, opponent_player);
                 break;
             case 2: //show score
-                // Show ships alive
-                // Show ships sunk
-                turn(current_player, opponent_player);
+                showStatus(current_player, opponent_player, ship_objects);
                 break;
             case 3: //use weapon
                 int user_weapon_choice = displayWeaponMenu(current_player);
@@ -114,7 +129,6 @@ public class Game {
                 //move fleet function
                 current_player.playerMoveFleet(fleet_direction_choice);
                 //go back
-                turn(current_player, opponent_player);
                 break;
             case 6: //Undo and redo move fleet
                 System.out.println("1. Undo Move Fleet");
@@ -140,6 +154,7 @@ public class Game {
                 String surrender_choice = input.next();
                 if (surrender_choice == "Y") {
                     //end game
+                    game_has_ended = true;
                     current_player.setSurrenderStatus();
                 }
                 else {
@@ -147,7 +162,11 @@ public class Game {
                 }
                 break;
         }
-
+        //check surrender variable for players --> end game
+        if (current_player.getSurrenderStatus()) {
+            game_has_ended = true;
+        }
+        return game_has_ended;
     }
 
     public void showTurnMenu() {
@@ -159,6 +178,61 @@ public class Game {
         System.out.println("5. Move Fleet");
         System.out.println("6. Undo/Redo Fleet Move");
         System.out.println("7. Surrender");
+    }
+
+    public void displayStartingMenu(newPlayer curr_player, ArrayList<newShip> ship_objects) {
+        int map_choice = 0;
+        String ship_names[] = {"Minesweeper", "Destroyer", "Battleship", "Submarine", "Spaceshuttle"};
+
+
+        System.out.println("How would you like to create your ship fleet?");
+        System.out.println("1. Create a random fleet");
+        System.out.println("2. Manually place fleet");
+
+        Scanner input = new Scanner(System.in);
+        int user_choice = input.nextInt();
+
+        switch (user_choice) {
+            case 1: //create random fleet
+                Fleet myFleet = new Fleet(ship_objects);
+                myFleet.placeFleet(curr_player);
+                break;
+            case 2: //manually place fleet
+
+                System.out.println("The following ships will be deployed based on a starting coordinate that faces either N, S, E, or W. ");
+
+                    for (int j = 0; j < ship_objects.size(); j++) {
+
+                        if (ship_objects.get(j) instanceof OrbitableShip) {
+                            map_choice = 2;
+                        }
+                        if (ship_objects.get(j) instanceof SubmersibleShip) {
+                            System.out.println("Which map do you want to deploy the submarine? Note: if there is a ship existing there on the ocean, your sub will automatically be placed underwater");
+                            System.out.println("1. Ocean");
+                            System.out.println("2. Underwater");
+                            map_choice = input.nextInt();
+                        }
+                        else {
+                            map_choice = 0;
+                        }
+                        boolean success;
+                        do {
+                            System.out.println("Place your " + ship_names[j]);
+                            System.out.print("X: ");
+                            int coord_choice_x = input.nextInt();
+                            System.out.print("Y: ");
+                            int coord_choice_y = input.nextInt();
+                            System.out.print("Ship direction: ");
+                            String direction_choice = input.next();
+                            success = curr_player.deployShip(ship_objects.get(j), coord_choice_x, coord_choice_y, direction_choice, map_choice);
+                        } while (!success);
+                    }
+                break;
+        }
+        System.out.println("You have successfully placed all your ships! View your ships on all 3 maps below:");
+        curr_player.player_maps.get(0).printOffensiveGrid();
+        curr_player.player_maps.get(1).printOffensiveGrid();
+        curr_player.player_maps.get(2).printOffensiveGrid();
     }
 
     public int displayMapMenu(newPlayer curr_player) {
@@ -192,66 +266,6 @@ public class Game {
         return user_boost_choice;
     }
 
-    public void displayStartingMenu(newPlayer curr_player) {
-
-        newShip sweeper = new Minesweeper();
-        newShip dest = new Destroyer();
-        newShip bat = new Minesweeper();
-        newShip sub = new Submarine();
-        newShip shut = new Spaceshuttle();
-        int map_choice = 0;
-        String ship_names[] = {"Minesweeper", "Destroyer", "Battleship", "Submarine", "Spaceshuttle"};
-        newShip ship_objects[] = {sweeper, dest, bat, sub, shut};
-
-        System.out.println("Let's get this game started! How would you like to create your ship fleet?");
-        System.out.println("1. Create a random fleet");
-        System.out.println("2. Manually place fleet");
-
-        Scanner input = new Scanner(System.in);
-        int user_choice = input.nextInt();
-
-        switch (user_choice) {
-            case 1: //create random fleet
-                map_choice = displayMapMenu(curr_player);
-//                Fleet player_fleet = new Fleet(ship_names);
-                //call placeFleet
-                break;
-            case 2: //manually place fleet
-
-                System.out.println("The following ships will be deployed based on a starting coordinate that faces either N, S, E, or W. ");
-
-                for (int i = 0; i < ship_names.length; i++) {
-                    for (int j = 0; j < ship_objects.length; j++) {
-                        if (i >= 0 && i <= 2) { //ships that are only placed on ocean
-                            map_choice = 0;
-                        }
-                        else if (i == 4) {
-                            map_choice = 2;
-                        }
-                        else if (i == 3) {
-                            System.out.println("Which map do you want to deploy the submarine? Note: if there is a ship existing there on the ocean, your sub will automatically be placed underwater");
-                            System.out.println("1. Ocean");
-                            System.out.println("2. Underwater");
-                            map_choice = input.nextInt();
-                        }
-                        System.out.println("Place your " + i);
-                        System.out.print("X: ");
-                        int coord_choice_x = input.nextInt();
-                        System.out.print("Y: ");
-                        int coord_choice_y = input.nextInt();
-                        System.out.print("Ship direction: ");
-                        String direction_choice = input.next();
-                        curr_player.deployShip(ship_objects[j], coord_choice_x, coord_choice_y, direction_choice, map_choice);
-                    }
-                }
-                break;
-            }
-            System.out.println("You have successfully placed all your ships! View your ships on all 3 maps below:");
-            curr_player.player_maps.get(0).printOffensiveGrid();
-            curr_player.player_maps.get(1).printOffensiveGrid();
-            curr_player.player_maps.get(2).printOffensiveGrid();
-    }
-
     public void createDisaster(newPlayer curr_player) {
         Random rand = new Random();
         int rand_disaster = rand.nextInt(6);
@@ -270,15 +284,26 @@ public class Game {
 
     }
 
-    public void getStatus() {
+    //prints out current score of both players
+    public void showStatus(newPlayer player1, newPlayer player2, ArrayList<newShip> ship_objects) {
+        int total_ships1 = player1.getPlayerMaps().get(0).getShipsAlive() + player1.getPlayerMaps().get(1).getShipsAlive() + player1.getPlayerMaps().get(2).getShipsAlive();
+        int total_ships2 = player2.getPlayerMaps().get(0).getShipsAlive() + player2.getPlayerMaps().get(1).getShipsAlive() + player2.getPlayerMaps().get(2).getShipsAlive();
+        int ship_sunk_1 = player1.getShipsSunk();
+        int ship_sunk_2 = player2.getShipsSunk();
 
-    }
 
-    public void attack(int x, int y) {
+        System.out.println("|-----------Current Score---------|");
+        System.out.println("|---------------------------------|");
+        System.out.println("|      " + player1.getName() + "  |  " + player2.getName() + "     |");
+        System.out.println("Total ships alive: " + total_ships1 + "   |   " + total_ships2);
+        System.out.println("Total ships sunk: " + ship_sunk_1 + "   |   " + ship_sunk_2);
+        System.out.println("|-----------Your Ship Healths---------|");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < ship_objects.size(); j++) {
+                System.out.println(ship_objects.get(j).getName() + ": " + player1.getPlayerMaps().get(i).ship_health.get(ship_objects.get(j)));
+            }
+        }
 
-    }
-
-    public void moveFleet() {
 
     }
 
--- src/edu/colorado/binarybuffs/Main.java
+++ src/edu/colorado/binarybuffs/Main.java
@@ -1,13 +1,43 @@
 package edu.colorado.binarybuffs;
-  
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
          //Ship ship = new Ship();
          //ship.show();
+        System.out.println("Welcome to the Battleship Game! In this 2 player game, you and your opponent will each be given a fleet of 5 ships \n" +
+                "that will be placed on 3 different maps - on the Ocean, Underwater, and in Space. Each player will take turns attacking \n" +
+                "the opponent's board, in which some ships may be attacked, some will go missed, and others can make or break the game. \n" +
+                "Additionally, each player will have a defensive and offensive grid on each map. The defensive grid shows locations of all \n" +
+                "your ships, while the offensive grid shows your attempted hits and misses on the opponent. Every time a ship is attacked, its \n" +
+                "health decreases. However, some ships are armored, and have a cpatain's quarters, which will take two hits to get fully attacked. \n" +
+                "The first player who has lost all their ships loses the game. Sounds easy enough right? Well, turns out there's more than one twist to the game...");
+        System.out.println("WEAPONS & BOOSTS--------------------------");
+        System.out.println("Each player will start with 2 weapons - a bomb and a sonar pulse. A sonar pulse may only be used after you have successfully sunk \n" +
+                "one of your opponents ships, for a maximum of 2 times. Additionally, when you have sunk one of your opponent's ships, your bomb \n" +
+                "will be upgraded to a space laser, which can be used to penetrate through the surface and shoot underwater as well. You are given \n" +
+                "a Lifesaver boost, which can be used to revive up to 2 sunken ships on your map.");
+        System.out.println("DISASTERS--------------------------");
+        System.out.println("There are 3 different types of disasters that can occur on your map - an Asteroid field, Ghost zone, or a Hurricane. The asteroid \n" +
+                "field will only occur in space, and may hit a spaceshuttle in the area. The ghost zone may appear either on the ocean or underwater \n" +
+                "where ghosts will take over your offensive grid and may scramble some of your data. The hurricane will only occur on the ocean, which \n" +
+                "will toss and turn ships caught on its borders. These disasters will be randomized before every turn.");
+        System.out.println("ANIMALS--------------------------");
+        System.out.println("What else could this game possibly have left? Oh yeah, animals! Before every turn, you may encounter a shark or a narwhal under one of your \n" +
+                "ships. A shark may bite off a part of your ship, while a narwhal will give you an extra use for your sonar pulse.");
+        System.out.println("---------------------------------");
+        System.out.println("Let's get this game started!");
+        System.out.println("\n");
 
-        Game new_game = new Game("Tanvi", "Sahand");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter name for player 1:");
+        String player1_name = keyboard.nextLine();
+        System.out.println("Enter name for player 2: ");
+        String player2_name = keyboard.nextLine();
+        Game new_game = new Game(player1_name, player2_name);
 
         new_game.turn(new_game.getPlayer1(), new_game.getPlayer2());
 
